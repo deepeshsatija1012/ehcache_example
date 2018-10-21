db7cb4f62220b72d750b1e13b9e4a102c6c871ca
deepeshsatija1012 <deepeshsatija@gmail.com>
2018-10-21 16:00:49 +0530

    adding commnts for links

--- src/main/java/com/ehcache/example/onheap/OnHeapMain.java
+++ src/main/java/com/ehcache/example/onheap/OnHeapMain.java
@@ -12,8 +12,14 @@ import org.ehcache.core.statistics.CacheStatistics;
 import org.ehcache.impl.internal.statistics.DefaultStatisticsService;
 
 public class OnHeapMain {
-
+    /**
+     * Ehcache management : http://www.ehcache.org/documentation/3.4/management.html
+     * Ehcache Documentation : http://www.ehcache.org/documentation/3.4/
+     * @param args
+     */
     public static void main(String[] args) {
+        // Cache Stats Example :
+        // https://stackoverflow.com/questions/40453859/how-to-get-ehcache-3-1-statistics/49303449#49303449
         StatisticsService statisticsService = new DefaultStatisticsService();
         CacheManager cacheManager = CacheManagerBuilder.newCacheManagerBuilder()
                 .withCache("preConfigured",
